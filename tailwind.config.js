9e2403dea6b8c66b70c98f5ee09d1fe24acf28f0
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2025-09-19 10:56:58 -0300

    Adicionando tailwind para o header

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",    // Aqui você indica onde estão seus arquivos HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
